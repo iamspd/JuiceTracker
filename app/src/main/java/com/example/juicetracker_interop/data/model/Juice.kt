3206b8b97a67089c4e77ef77c7539be5bf4cb4ce
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-04-28 20:20:37 -0400

    Add strings and model class

---
+++ app/src/main/java/com/example/juicetracker_interop/data/model/Juice.kt
@@ -0,0 +1,28 @@
+package com.example.juicetracker_interop.data.model
+
+import android.graphics.Color
+import androidx.annotation.StringRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.juicetracker_interop.R
+
+@Entity
+data class Juice(
+    @PrimaryKey val id: Int = 0,
+    val name: String,
+    val description: String,
+    val color: String,
+    val rating: Int
+)
+
+enum class JuiceColor(
+    val color: Int,
+    @StringRes val label: Int
+) {
+    Red(Color.RED, R.string.red),
+    Blue(Color.BLUE, R.string.blue),
+    Green(Color.GREEN, R.string.green),
+    Cyan(Color.CYAN, R.string.cyan),
+    Yellow(Color.YELLOW, R.string.yellow),
+    Magenta(Color.MAGENTA, R.string.magenta)
+}
