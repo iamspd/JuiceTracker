f6989d52fce9ee0d9223050c82a29508d8abc7e1
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-04-30 11:33:20 -0400

    Add Play Services Ads dependency and meta-data in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // Play Services Ads
+    implementation(libs.play.services.ads)
 }
